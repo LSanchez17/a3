73f077a59d237256555ec8cea1a79f60a808da1a
LSanchez17 <galaxyplayeremail@gmail.com>
2016-10-30 23:40:27 -0400

    fixed
    done for now

--- a3/views/login.php
+++
@@ -1,9 +0,0 @@
-<?php include('elements/header.php');?>
-<div class="container">
-	<div class="page-header">
-   <h1> the Login View </h1>
-   <?php echo $numbers ?>
-  </div>
-</div>
-<?php include('elements/footer.php');?>
-
